e938a7ddc24f6424b0b852e89cc062a21c76a606
wenxiu.su <wenxiu.su@beibei.com>
2021-02-03 14:40:05 +0800

    update

--- bin/deploy/build.sh
+++ bin/deploy/build.sh
@@ -29,6 +29,7 @@ if [ -s "./package.json" ]; then
     while read line
 
     do
+    echo "line : $line"
     if [[ $line =~ "name" ]]; then
 
         # echo $line
@@ -53,59 +54,60 @@ fi
 
 echo "-----工程名：$PROJECT_NAME-----"
 
-# 2、获取配置文件 按照配置安装对应的依赖包
+# # 2、获取配置文件 按照配置安装对应的依赖包
 
-fileName="package-config-test"
+# fileName="package-config-test"
 
-gitcloneurl="https://github.com/AUW-su/package-config-test.git"
+# gitcloneurl="https://github.com/AUW-su/package-config-test.git"
 
 
-git clone "$gitcloneurl"
+# git clone "$gitcloneurl"
 
-#  chmod u+x *.sh
+# #  chmod u+x *.sh
 
-# 判断配置文件是否有内容，在[   ]内要有空格
-if [ -s "./${fileName}/.config" ]; then
+# # 判断配置文件是否有内容，在[   ]内要有空格
+# if [ -s "./${fileName}/.config" ]; then
 
-    echo "-----依赖包版本配置文件有内容-----"
+#     echo "-----依赖包版本配置文件有内容-----"
 
-    # 开始读配置文件
-    while read line
+#     # 开始读配置文件
+#     while read line
 
-    do
-    echo ${line}
-    echo $PROJECT_NAME
-    # if [[ $line =~ "all" ]] || [[ ! -z $PROJECT_NAME && $line =~ $PROJECT_NAME ]]; then
-    #     echo "依赖包版本配置文件当前一行的内容：$line"
+#     do
+#     echo ${line}
+#     echo $PROJECT_NAME
+#     # if [[ $line =~ "all" ]] || [[ ! -z $PROJECT_NAME && $line =~ $PROJECT_NAME ]]; then
+#     #     echo "依赖包版本配置文件当前一行的内容：$line"
 
-    #     line="$(echo "${line}" | tr -d '[:space:]')" # 去掉空格
+#     #     line="$(echo "${line}" | tr -d '[:space:]')" # 去掉空格
 
-    #     IFS=":"
+#     #     IFS=":"
 
-    #     array=($line)
+#     #     array=($line)
 
-    #     echo "-----要安装的依赖包信息：${array[1]}-----"
+#     #     echo "-----要安装的依赖包信息：${array[1]}-----"
 
-    #     npm install ${array[1]}  --registry https://registry.npm.taobao.org
-    # fi
+#     #     npm install ${array[1]}  --registry https://registry.npm.taobao.org
+#     # fi
     
-    done < "./${fileName}/.config"
-fi
+#     done < "./${fileName}/.config"
+# fi
 
-# 删除本次脚本执行新增的文件
+# # 3、删除本次脚本执行新增的文件
 rm -rf "./test.txt"
-rm -rf "./${fileName}"
 
-# 判断是否有需要提交的文件的文件
-if [[ "${GIT_STATUS}" == *"nothing to commit"* ]]; then
-    echo "-----自动更新依赖包版本 没有可提交的内容-----";
-else
-    echo "-----自动更新依赖包版本 自动提交diff-----"
-    git add ./package.json
-    git add ./package-lock.json
-    git commit --no-verify -m "build.sh auto commit package.json & package-lock.json";
-    git push
-fi
+# rm -rf "./${fileName}"
+
+# # 4、判断是否有需要提交的文件的文件
+# if [[ "${GIT_STATUS}" == *"nothing to commit"* ]]; then
+#     echo "-----自动更新依赖包版本 没有可提交的内容-----";
+# else
+#     echo "-----自动更新依赖包版本 自动提交diff-----"
+#     git add ./package.json
+#     git add ./package-lock.json
+#     git commit --no-verify -m "build.sh auto commit package.json & package-lock.json";
+#     git push
+# fi
 
 #----------------------------------------
 
